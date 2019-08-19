1f99d566a9745c3915e02211b74107b8eda82c50
xavier <xavier.kress@gmail.com>
2019-08-19 19:57:48 +0200

    add setterInjectedController Test

---
+++ src/test/java/com/demospring/xavierkress/dependencyinjection/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package com.demospring.xavierkress.dependencyinjection.controllers;
+
+import com.demospring.xavierkress.dependencyinjection.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SetterInjectedControllerTest {
+
+    private SetterInjectedController setterInjectedController;
+
+    @Before
+    public void setUp() throws Exception {
+        this.setterInjectedController = new SetterInjectedController();
+        this.setterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_WORLD, setterInjectedController.sayHello());
+    }
+}
