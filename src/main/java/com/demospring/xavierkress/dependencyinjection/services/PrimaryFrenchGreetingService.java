ad3bd6d97052954c9cb985fe5d31fdb2917db6ab
xavier <xavier.kress@gmail.com>
2019-08-20 19:34:20 +0200

    use profiles for loading different beans

--- src/main/java/com/demospring/xavierkress/dependencyinjection/services/PrimaryFrenchGreetingService.java
+++ src/main/java/com/demospring/xavierkress/dependencyinjection/services/PrimaryFrenchGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile("fr")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryFrenchGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello From primary greeting service";
+        return "Bonjour depuis le service de salutation primaire";
     }
 }
